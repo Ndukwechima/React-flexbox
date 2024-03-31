c9c24c41410b729d27754686d033ec8a92a8db82
Ndukwe Chima <chimandukwe.r@gmail.com>
2024-03-31 14:47:36 +0100

    added flexbox assignment

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+// import Flexbox from './Flexbox';
+import Assignment from "./Assignment";
+// import Assign from "./Assign";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Flexbox/> */}
+      <Assignment />
+      {/* <Assign /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Assignment.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Assignment.css";
+
+function Assign() {
+  return (
+    <section className="parent-container">
+      <section className="grid-container">
+        <div className="book1">Book 1</div>
+        <div className="book2">Book 2</div>
+        <div className="book3">Book 3</div>
+        <div className="book4">Book 4</div>
+        <div className="book5">Book 5</div>
+        <div className="book6">Book 6</div>
+        <div className="book7">Book 7</div>
+        <div className="book8">Book 8</div>
+        <div className="book9">Book 9</div>
+        <div className="book10">Book 10</div>
+        <div className="book11">Book 11</div>
+        <div className="book12">Book 12</div>
+        <div className="book13">Book 13</div>
+        <div className="book14">Book 14</div>
+        <div className="book15">Book 15</div>
+        <div className="book16">Book 16</div>
+      </section>
+    </section>
+  );
+}
+
+export default Assign;
